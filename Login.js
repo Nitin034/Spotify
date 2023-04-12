c503b2ab21dc93ef82fa80449e81c45d34313c92
Nitin Kumar <93970891+Nitin034@users.noreply.github.com>
2023-04-12 22:13:21 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=b596edafa6854d5294bd99eaaf5f2de8&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function login() {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login With Spotify
+      </a>
+    </Container>
+  )
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+// const lyricsFinder = require("lyrics-finder")
+const SpotifyWebApi = require("spotify-web-api-node")
+
+ const app = express()
+ app.use(cors())
+ app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.post("/refresh", (req, res) => {
+//   const refreshToken = req.body.refreshToken
+//   const spotifyApi = new SpotifyWebApi({
+//     redirectUri: "http://localhost:3000",
+//     clientId: "b596edafa6854d5294bd99eaaf5f2de8",
+//     clientSecret: "492f115e30f349ec9e42391db34d5f38",
+//     refreshToken,
+//   })
+
+//   spotifyApi
+//     .refreshAccessToken()
+//     .then(data => {
+//       res.json({
+//         accessToken: data.body.accessToken,
+//         expiresIn: data.body.expiresIn,
+//       })
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.sendStatus(400)
+//     })
+// })
+
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "b596edafa6854d5294bd99eaaf5f2de8",
+    clientSecret: "492f115e30f349ec9e42391db34d5f38",
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+// app.get("/lyrics", async (req, res) => {
+//   const lyrics =
+//     (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+//   res.json({ lyrics })
+// })
+
+app.listen(3001)
